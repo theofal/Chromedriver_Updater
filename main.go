2009fc9ea8d10dd565eccb2763593e205c161e3e
theofal <46688217+theofal@users.noreply.github.com>
2022-08-13 17:12:33 +0200

    main package rearrangement

--- main.go
+++ main.go
@@ -16,40 +16,6 @@ import (
 	"strings"
 )
 
-func main() {
-	logger := zaplogger.InitLogger(zapcore.DebugLevel, zapcore.DebugLevel).Sugar()
-
-	// flags:
-	// -v (--version) get the latest version from a given major version (int)
-	// -o (--file) set chromedriver path manually (default /usr/local/bin) (string)
-	file := flag.String("f", viper.GetString("configPath"), "Specify the folder where the binary will be installed")
-	install := flag.Bool("i", false, "Configure the chromedriver path")
-	version := flag.Int("v", 0,
-		"Specify the major version of the chromedriver (default: 0 = Same as installed Google chrome version)")
-	flag.Parse()
-
-	if *version < 0 {
-		logger.Fatalf("Version number cannot be negative.")
-	}
-
-	if *file == "" {
-		if !*install {
-			configureAppFile(logger)
-		}
-		if *install {
-			err := installViper()
-			if err != nil {
-				logger.Fatalf("An error occurred while configuring file: %v", err)
-			}
-		}
-		*file = viper.GetString("configPath")
-		logger.Infof("File path set to %s", *file)
-	}
-
-	app := src.NewApp(logger)
-	app.InitApp(*version, filepath.Clean(*file))
-}
-
 func initViper() error {
 	viper.SetConfigName("config")                                             // name of config file (without extension)
 	viper.SetConfigType("yaml")                                               // REQUIRED if the config file does not have the extension in the name
@@ -110,4 +76,39 @@ func configureAppFile(logger *zap.SugaredLogger) {
 		i = true
 		logger.Infof("Config file found, path: %s", viper.GetString("configPath"))
 	}
+}
+
+func main() {
+	logger := zaplogger.InitLogger(zapcore.DebugLevel, zapcore.DebugLevel).Sugar()
+
+	// -f (--file) set chromedriver path manually (default /usr/local/bin) (string)
+	file := flag.String("f", viper.GetString("configPath"), "Specify the folder where the binary will be installed")
+	// -i (--install) configure the chromedriver path
+	install := flag.Bool("i", false, "Configure the chromedriver path")
+	// -v (--version) get the latest version from a given major version (int)
+	version := flag.Int("v", 0,
+		"Specify the major version of the chromedriver (default: 0 = Same as installed Google chrome version)")
+
+	flag.Parse()
+
+	if *version < 0 {
+		logger.Fatalf("Version number cannot be negative.")
+	}
+
+	if *file == "" {
+		if !*install {
+			configureAppFile(logger)
+		}
+		if *install {
+			err := installViper()
+			if err != nil {
+				logger.Fatalf("An error occurred while configuring file: %v", err)
+			}
+		}
+		*file = viper.GetString("configPath")
+		logger.Infof("File path set to %s", *file)
+	}
+
+	app := src.NewApp(logger)
+	app.InitApp(*version, filepath.Clean(*file))
 }
